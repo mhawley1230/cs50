8f467a0b0fc2819e6b1a7cb74e7af30947675a56
Mike Hawley <mhawley1230@gmail.com>
2017-03-17 14:48:49 +0000

    finally completed greedy

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -19,35 +20,30 @@ int main(void)
     }
     while (change < 0);
     
-    int remainder = change * 100;
-    printf("%.2f times 100 = %i\n", change, remainder);
+    int remainder = (int) roundf(change * 100);
     
     while (remainder >= quarter)
     {
         coin_count++;
         remainder -= quarter;
-        printf("Quarter subtracted, %i remaining\n", remainder);
     }
     
     while (remainder >= dime)
     {
         coin_count++;
         remainder -= dime;
-        printf("Dime subtracted, %i remaining\n", remainder);
     }
     
     while (remainder >= nickel)
     {
         coin_count++;
         remainder -= nickel;
-        printf("Nickel subtracted, %i remaining\n", remainder);
     }
     
     while (remainder >= penny)
     {
         coin_count++;
         remainder -= penny;
-        printf("Penny subtracted, %i remaining\n", remainder);
     }
     
     printf("%i\n", coin_count);
