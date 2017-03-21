5dcab3c494ca3d1328d7951f5bb9a58a32de7066
Mike Hawley <mhawley1230@gmail.com>
2017-03-21 17:01:43 +0000

    caesar complete and submitted

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,24 +1,52 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, string argv[])
 {
+    // error for incorrect number of command-line arguments
     if (argc != 2)
     {
-        eprintf("Usage: ./caesar k");
+        printf("Usage: ./caesar k");
         return 1;
     }
-    
+       
     printf("plaintext: ");
     string s = get_string();
     
+    printf("ciphertext: ");
+    
+    // convert string argv[1] to int
+    int k = atoi(argv[1]);
+    
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (('A' <= s[i] && s[i] <= 'Z') ||
-            ('a' <= s[i] && s[i] <= 'z'))
+        char l = s[i];
+        // if letter is capital
+        if (l >= 'A' && l <= 'Z')
         {
-            printf("%c", s[i + (int) argv[1]]);
+            // condition for wrapping capital alphabet
+            while (l + k > 'Z')
+            {
+                l -= 26;
+            }
+            printf("%c", l + k);
+        }
+        // if letter is lowercase
+        else if (l >= 'a' && l <= 'z')
+        {
+            // condition for wrapping lowercase alphabet
+            while (l + k > 'z')
+            {
+                l -= 26;
+            }
+            printf("%c", l + k);
+        }    
+        else
+        {
+            printf("%c", l);
         }
     }
+    printf("\n");
 }
