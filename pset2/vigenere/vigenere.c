06f9d519435152fc746e910240854b66d8ce2d41
Mike Hawley <mhawley1230@gmail.com>
2017-04-04 23:08:06 +0000

    vigenere complete, submitted

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -3,60 +3,100 @@
 #include <string.h>
 #include <ctype.h>
 #define ALPHABET_LENGTH 26
-#define START_OF_UPPER_ALPHABET 65
-#define START_OF_LOWER_ALPHABET 97
 
-int encrypt(string key);
+int vigenere(string message, string key);
 
 int main(int argc, string argv[])
 {
+    
+    // if vigenere is ran with "./vigenere" or more than two functions
     if (argc != 2)
     {
-        printf("Usage: ./caesar k");
+        // give prompt to run correctly
+        printf("Usage: ./vigenere s");
+        
+        // exit program
         return 1;
     }
     
-    encrypt(argv[1]);
-    printf("\n");
-}
-
-int encrypt(string key)
-{
-    printf("plaintext: ");
-    string s = get_string();
+    // set the key string to a variable
+    string key = argv[1];
     
-    // int input_letter_offset = 0;
-    // int key_offset = 0;
-    for (int i = 0, n = strlen(s); i < n; i++)
+    // iterate through key to check for non-letter characters
+    for(int i = 0, j = strlen(key); i < j; i++)
     {
-        int key_letter_position = 0;
-        char c = s[i];
-        if (isalpha(c))
+        if (!isalpha(key[i]))
         {
-            if (key_letter_position < strlen(key))
-            {
-                key_letter_position++;
-            }
-            else
-            {
-                key_letter_position -= strlen(key);
-            }
-            
-            printf("the %i'th letter of the key string is %c\n", key_letter_position, key[i]);
-            if (c >= START_OF_UPPER_ALPHABET && c <= START_OF_UPPER_ALPHABET + ALPHABET_LENGTH)
-            {
-                // key_offset = toupper(key[i]) - START_OF_UPPER_ALPHABET;
-                // input_letter_offset = c - START_OF_UPPER_ALPHABET;
-                // printf("%c is going to be added to %c", key_offset, input_letter_offset);
-                // c = (key_offset + input_letter_offset) % ALPHABET_LENGTH;
-                // printf("%c", c + START_OF_UPPER_ALPHABET);
-            }
-        }
-        else
-        {
-            printf("%c", c);
+            // give error message, abort
+            printf("please only use alphanumeric letters in key");
+            return 1;
         }
     }
     
+    // prompt user for string to encrypt
+    printf("plaintext: ");
+    
+    // get input string
+    string s = get_string();
+    vigenere(s, key);
+    
+    printf("\n");
+    return 0;
+}
+
+int vigenere(string message, string key)
+{
+    printf("ciphertext: ");
+    
+    // get length of key
+    int key_length = strlen(key);
+    
+    // iterate through each character in message, set counter to iterate through key
+    for (int i = 0, key_counter = 0, n = strlen(message); i < n; i++)
+    {
+        // check for punctuation and spaces in message
+        if (!isalpha(message[i]))
+        {
+            // return punctuation and spaces unchanged
+            printf("%c", message[i]);
+        }
+        else
+        {
+            // use counter to get char in key, standardize and subtract offset to add
+            char k = toupper(key[key_counter]) - 'A';
+            
+            // check if message[i] is upper or lower case
+            if (isupper(message[i]))
+            {
+                // while loop to wrap upper case alphabet cyclically
+                while (message[i] + k > 'Z')
+                {
+                    k -= ALPHABET_LENGTH;
+                }
+                // print upper case letter plus key character within upper case alphabet
+                printf("%c", message[i] + k);
+            }
+            else
+            {
+                // while loop to wrap lower case alphabet cyclically
+                while (message[i] + k > 'z')
+                {
+                    k -= ALPHABET_LENGTH;
+                }
+                // print lower case letter plus key character within lower case alphabet
+                printf("%c", message[i] + k);
+            }
+            
+            // cycle through characters in key only if using key to encrypt
+            if (key_counter < key_length - 1)
+            {
+                key_counter++;
+            }
+            else
+            {
+                key_counter = 0;
+            }
+        }
+    }
     return 0;
 }
