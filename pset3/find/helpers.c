dbb46a24b74690bca20cab07624c472ac439af31
Mike Hawley <mhawley1230@gmail.com>
2017-04-07 19:51:28 +0000

    completed bubble sort, search is next

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -5,8 +5,12 @@
  */
  
 #include <cs50.h>
+#include <stdio.h>
 
 #include "helpers.h"
+
+// the upper limit of possible values generated
+#define LIMIT 65536
 
 /**
  * Returns true if value is in array of n values, else false.
@@ -14,6 +18,7 @@
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
+    
     return false;
 }
 
@@ -23,5 +28,31 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
-    return;
+    
+    // set swap counter to non-zero number
+    int swap_count = -1;
+    
+    // do n times
+    for (int i = 0; i < n; i++)
+    {
+        // set swap_count to 0 every iteration
+        swap_count = 0;
+        
+        // if two values next to each other are not in order
+        if (values[i] < values[i + 1])
+        {
+            // swap lower numbers for higher numbers
+            int temp = values[i];
+            values[i] = values[i + 1];
+            values[i + 1] = temp;
+            
+            //increment swap count
+            swap_count++;
+        }
+        
+        if (swap_count != 0)
+        {
+            sort(values, n - 1);
+        }
+    }
 }
