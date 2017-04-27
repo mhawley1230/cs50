86a4c2ec563a0ee2f575add821fa491c2d628f25
Mike Hawley <mhawley1230@gmail.com>
2017-04-27 19:14:38 +0000

    trying less comfortable first

--- pset4/resize/resize.c
+++ pset4/resize/resize.c
@@ -10,18 +10,15 @@
 int main(int argc, char *argv[])
 {
     // ensure proper usage
-    if (argc != 4)
+    if (argc != 3)
     {
-        fprintf(stderr, "Usage: ./resize n infile outfile\n");
+        fprintf(stderr, "Usage: ./copy infile outfile\n");
         return 1;
     }
-    
-    // set resize scale
-    float resize = atof(argv[1]);
 
     // remember filenames
-    char *infile = argv[2];
-    char *outfile = argv[3];
+    char *infile = argv[1];
+    char *outfile = argv[2];
 
     // open input file 
     FILE *inptr = fopen(infile, "r");
@@ -57,21 +54,15 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
-    
-    // determine padding for scanlines
-    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-    
-    // recalc biSizeImage for resizing
-    bi.biSizeImage = ((sizeof(RGBTRIPLE) * (bi.biWidth * resize) + padding) * (abs(bi.biHeight) * resize));
-    
-    // recalc bfSize for resizing
-    bf.bfSize = bi.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
 
     // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
     // write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
