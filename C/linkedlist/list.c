3f59719d38d9bad8c431c3bda33b9341667da9ca
Mike Hawley <mhawley1230@gmail.com>
2017-05-08 20:10:43 +0000

    creating a search function

--- C/linkedlist/list.c
+++ C/linkedlist/list.c
@@ -10,6 +10,9 @@ typedef struct node {
 }
 node;
 
+// declare functions to interact with list
+bool search(node* head, int val);
+
 // set head node to NULL to keep track of first element in list
 struct node *head = NULL;
 
@@ -20,14 +23,43 @@ int main(void)
 {
     // malloc first node
     struct node *list = (struct node*)malloc(sizeof(struct node));
+    
     // set first node data
     list->data = 6;
+    
     // first node isn't pointing at anything
     list->next = NULL;
+    
+    // set global pointers to first element
     head = current = list;
+    
+    // new node
     struct node *another = (struct node*)malloc(sizeof(struct node));
+    
+    // new node's data
     another->data = 7;
-    list->next = another;
-    current = current->next;
-    printf("%d\n", current->data);
+    
+    // // set first node to point to new node
+    // list->next = another;
+    
+    // // change what current is pointing at, while still keeping track of head
+    // current = current->next;
+    
+    printf("head node's value is %d\n", head->data);
+    printf("element 1's value is %d\n", current->data);
+    
+    // search for an element
+    if (search(list, 7))
+    {
+        printf("element found");
+    }
+    else
+    {
+        printf("element not found");
+    }
+}
+
+bool search(node* head, int val);
+{
+    
 }
